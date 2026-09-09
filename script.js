1fe9e4708b1d63870a61292b980b534b026dfa22
snathaly15-dot <s.nathaly15@escola.pr.gov.br>
2026-09-09 17:26:27 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const botao = document.querySelector("#botaoMensagem");
+const mensagem = document.querySelector("#mensagemInterativa");
+    botao.addEventListener ("click", mostrarMensagem);
+    function mostrarMensagem(){
+        mensagem.textContent =
+            "Você acabou de criar uma interação com JavaScript!"
+    }
