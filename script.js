733b04db22acd9bd5e637365d618aeffd48079db
snathaly15-dot <s.nathaly15@escola.pr.gov.br>
2026-09-23 17:07:26 -0300

    atualizei meu site

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const botao = document.querySelector("#botaoMensagem");
-const mensagem = document.querySelector("#mensagemInterativa");
-    botao.addEventListener ("click", mostrarMensagem);
-    function mostrarMensagem(){
-        mensagem.textContent =
-            "Você acabou de criar uma interação com JavaScript!"
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+function curtir (){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtiu === false){
+        contador.textContent++;
+        curtiu = true;
+    }else{
+        contador.textContent --;
+        curtiu = false;
     }
+});
